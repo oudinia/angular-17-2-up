6b52335dedc111844544fc73f2fe7903eddd8e9f
Oussama Dinia <odinia@groupeisagri.com>
2024-02-21 12:06:58 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, computed, inject, WritableSignal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AppService, Product} from "./app.service";
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,21 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'practice-app';
+  title = 'practice-app-23';
+  appService = inject(AppService);
+
+  constructor() {}
+
+  addProduct() {
+    const productId = Math.floor(Math.random() * (100 - 4 + 1)) + 4;
+    const productName = `product ${productId}`;
+    this.appService.addProduct({id: productId, name: productName});
+  }
+
+  get products() {
+    return this.appService.products;
+  }
+
+  productCount =
+    computed(() => this.appService.products().length)
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,22 @@
+import {Injectable, signal} from "@angular/core";
+
+export interface Product {
+  id: number;
+  name: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  products = signal<Product[]>([]);
+  constructor() {
+  }
+
+  addProduct(product: Product) {
+    this.products.update(products => [...products, product]);
+  }
+
+
+}
